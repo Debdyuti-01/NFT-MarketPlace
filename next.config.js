3400d3322791b17b9d5b76b244770fdce80b81f2
Debdyuti <das.debdyuti01@gmail.com>
2023-05-04 15:42:30 -0700

    Changes for NFT project

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["debdyuti-nft-marketplace.infura-ipfs.io"],
+    formats: ["image/webp"],
+  },
+};
+
+/* @type {import('next').nextConfig} 
+const nextConfig= {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["debdyuti-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
+   //formats: ["image/webp"],
+   
+  },
+};
+module.exports = nextConfig;*/
